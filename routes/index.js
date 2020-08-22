4a3660eca05ced0fc74276c7814e461b3d0d6717
hrithikv <35966198+hrithikv@users.noreply.github.com>
2020-08-22 19:09:43 -0400

    Create index.js

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+var monk = require('monk');
+var app = require('../app');
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  console.log(req.cookies['connect.sid']);
+  res.render('index', { title: 'Realtime Whiteboard + Chat', name: "Hrithik" });
+});
+
+module.exports = router;
