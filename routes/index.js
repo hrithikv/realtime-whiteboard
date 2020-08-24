9d49b08d28b542bc612ff30c291c55fe247d6c8a
hrithikv <35966198+hrithikv@users.noreply.github.com>
2020-08-24 04:28:27 -0400

    Updated style.css to add containers and grid.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,6 @@ var router = express.Router();
 var monk = require('monk');
 var app = require('../app');
 
-/* GET home page. */
 router.get('/', function(req, res) {
   console.log(req.cookies['connect.sid']);
   res.render('index', { title: 'Realtime Whiteboard + Chat', name: "Hrithik" });
