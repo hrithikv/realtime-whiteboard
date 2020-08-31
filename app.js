e1c002f1332818260e6f11e81f3d32e2bb6ec80b
hrithikv <35966198+hrithikv@users.noreply.github.com>
2020-08-31 07:07:26 -0400

    transfer files to repo

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 
 const express = require('express');
 const path = require('path');
-// const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 ...
@@ -9,27 +8,20 @@ const cookieParser = require('cookie-parser');
 app.use('/', index);
 app.use('/systemadmin', systemAdminRoutes);
 
-// catch 404 and forward to error handler
 app.use((req, res, next) => {
   const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-// error handler
 app.use((err, req, res) => {
-  // next is the last argument in above function
-  // set locals, only providing error in development
-  // res.locals.message = err.message;
-  // res.locals.error = req.app.get('env') === 'development' ? err : {};
-  debug(err); // nothing logs here 
+  debug(err); 
   debug(err.message);
   debug(err.name);
   debug(err.errorCode);
   debug(err.stack);
-  // render the error page
   res.status(err.status || 500);
-  res.send({error: err.message}); //this never gets called
+  res.send({error: err.message}); 
 });
 
 module.exports = app;
